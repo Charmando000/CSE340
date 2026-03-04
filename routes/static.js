a7d05f3620242508862e7f8c0d1c5a6f7290bf3f
Charmando000 <elescogido0310@gmail.com>
2026-03-04 15:41:36 -0300

    assigment

--- routes/static.js
+++ routes/static.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require("path")
 const router = express.Router();
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
-
+router.use(express.static(path.join(__dirname, "..", "public")));
+router.use("/css", express.static(path.join(__dirname, "..", "public", "css")));
+router.use("/js", express.static(path.join(__dirname, "..", "public", "js")));
+router.use("/images", express.static(path.join(__dirname, "..", "public", "images")));
+router.use("/upgrades", express.static(path.join(__dirname, "..", "public", "images", "upgrades")));
+router.use("/site", express.static(path.join(__dirname, "..", "public", "images", "site")));
 module.exports = router;
 
 
