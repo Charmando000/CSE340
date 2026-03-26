cd91ef962bf87bd7eef94ccfc5110806c36601af
Charmando000 <elescogido0310@gmail.com>
2026-03-26 11:33:40 -0300

    form

---
+++ routes/accountRoute.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const router = new express.Router()
+const regValidate = require('../utilities/account-validation')
+
+const utilities = require("../utilities/")
+const accountController = require("../controllers/accountController")
+//login
+router.get("/login/", utilities.handleErrors(accountController.buildLogin)
+)
+//register
+router.get(
+  "/register",
+  utilities.handleErrors(accountController.buildRegister)
+)
+
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,12 +5,17 @@
 /* ***********************
  * Require Statements
  *************************/
+const session = require("express-session")
+const pool = require('./database/')
 const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
 const express = require("express")
 expressLayouts = require("express-ejs-layouts")
 const utilities = require("./utilities/")
 const env = require("dotenv").config()
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
+
 
 const app = express()
 const static = require("./routes/static")
@@ -23,12 +28,37 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 app.use("/inv", utilities.handleErrors(inventoryRoute))
 app.use(express.static("public"))
+app.use("/account", accountRoute)
 
 //Index Route
 app.get("/", function(req, res) {
