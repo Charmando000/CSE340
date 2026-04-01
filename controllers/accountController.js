f4850b9482763af9b90f71f959483c3732b7cc6e
Charmando000 <elescogido0310@gmail.com>
2026-04-01 13:34:13 -0300

    resgister

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -39,7 +39,7 @@ async function registerAccount(req, res) {
     hashedPassword = await bcrypt.hashSync(account_password, 10)
   } catch (error) {
     req.flash("notice", 'Sorry, there was an error processing the registration.')
-    res.status(500).render("account/register", {
+    return res.status(500).render("account/register", {
       title: "Registration",
       nav,
       errors: null,
@@ -58,10 +58,7 @@ async function registerAccount(req, res) {
       "notice",
       `Congratulations, you\'re registered ${account_firstname}. Please log in.`
     )
-    res.status(201).render("account/login", {
-      title: "Login",
-      nav,
-    })
+    return res.redirect("/account/login")
   } else {
     req.flash("notice", "Sorry, the registration failed.")
     res.status(501).render("account/register", {
